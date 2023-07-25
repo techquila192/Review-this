c2d17b2a2e8d4c4a61a98b580d8ef72ba1854244
techquila42 <55506290+techquila42@users.noreply.github.com>
2023-07-25 15:06:31 +0530

    registration routes added

---
+++ controller/user_register.route.js
@@ -0,0 +1,7 @@
+const express=require('express');
+const router=express.Router();
+
+router.post('/user-register',(req,res)=>{
+    const email=req.body.email;
+    const fullName=req.body.fullName;
+});
